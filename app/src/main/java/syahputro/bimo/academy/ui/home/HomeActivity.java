78a19ad200c9ee5f89571b2fc9f0b41940f54db4
bimosyah <bimosyahputro88@gmail.com>
2020-01-02 21:56:44 +0700

    asset

--- app/src/main/java/syahputro/bimo/academy/ui/home/HomeActivity.java
+++ app/src/main/java/syahputro/bimo/academy/ui/home/HomeActivity.java
@@ -1,22 +1,45 @@
 package syahputro.bimo.academy.ui.home;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import syahputro.bimo.academy.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.action_bookmark:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
