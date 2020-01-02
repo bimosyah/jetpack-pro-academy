35e52c95909237998260b67a11d2c6ebe5dd920c
bimosyah <bimosyahputro88@gmail.com>
2020-01-02 22:08:14 +0700

    asset

--- app/src/main/java/syahputro/bimo/academy/ui/home/HomeActivity.java
+++ app/src/main/java/syahputro/bimo/academy/ui/home/HomeActivity.java
@@ -16,20 +16,14 @@ public class HomeActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.action_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.action_bookmark:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-            }
-            return false;
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                return true;
+            case R.id.action_bookmark:
+                return true;
         }
+        return false;
     };
 
     @Override
