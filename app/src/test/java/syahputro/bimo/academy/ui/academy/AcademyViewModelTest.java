c7993aa4864ac12c6dcfb38aa1fadc53ccbb3594
bimosyah <bimosyahputro88@gmail.com>
2020-01-06 17:46:59 +0700

    Pengujian Repository dan Injection

--- app/src/test/java/syahputro/bimo/academy/ui/academy/AcademyViewModelTest.java
+++ app/src/test/java/syahputro/bimo/academy/ui/academy/AcademyViewModelTest.java
@@ -6,20 +6,28 @@ import org.junit.Test;
 import java.util.List;
 
 import syahputro.bimo.academy.data.CourseEntity;
+import syahputro.bimo.academy.data.source.AcademyRepository;
+import syahputro.bimo.academy.utils.FakeDataDummy;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class AcademyViewModelTest {
     private AcademyViewModel viewModel;
+    private AcademyRepository academyRepository = mock(AcademyRepository.class);
 
     @Before
     public void setUp() {
-        viewModel = new AcademyViewModel();
+        viewModel = new AcademyViewModel(academyRepository);
     }
 
     @Test
     public void getCourses() {
+        when(academyRepository.getAllCourses()).thenReturn(FakeDataDummy.generateDummyCourses());
         List<CourseEntity> courseEntities = viewModel.getCourses();
+        verify(academyRepository).getAllCourses();
         assertNotNull(courseEntities);
         assertEquals(5, courseEntities.size());
     }
--- app/src/test/java/syahputro/bimo/academy/ui/bookmark/BookmarkViewModelTest.java
+++ app/src/test/java/syahputro/bimo/academy/ui/bookmark/BookmarkViewModelTest.java
@@ -6,20 +6,28 @@ import org.junit.Test;
 import java.util.List;
 
 import syahputro.bimo.academy.data.CourseEntity;
+import syahputro.bimo.academy.data.source.AcademyRepository;
+import syahputro.bimo.academy.utils.FakeDataDummy;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class BookmarkViewModelTest {
     private BookmarkViewModel viewModel;
+    private AcademyRepository academyRepository = mock(AcademyRepository.class);
 
     @Before
     public void setUp() {
-        viewModel = new BookmarkViewModel();
+        viewModel = new BookmarkViewModel(academyRepository);
     }
 
     @Test
     public void getBookmark() {
+        when(academyRepository.getBookmarkedCourses()).thenReturn(FakeDataDummy.generateDummyCourses());
         List<CourseEntity> courseEntities = viewModel.getBookmarks();
+        verify(academyRepository).getBookmarkedCourses();
         assertNotNull(courseEntities);
         assertEquals(5, courseEntities.size());
     }
